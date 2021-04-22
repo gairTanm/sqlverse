15a9ec830b3e62b023037d551371c662ec87f518
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-04-23 05:20:22 +0530

    extract out routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<App />
-		</ChakraProvider>
+		<Router>
+			<ChakraProvider>
+				<App />
+			</ChakraProvider>
+		</Router>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
