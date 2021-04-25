61e4b8d6b70a2ee260335f765fcce17fa57b6694
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-04-25 12:12:24 +0530

    team[wip]

--- client/src/components/team.js
+++ client/src/components/team.js
@@ -1,10 +1,79 @@
-import { Box, Text } from "@chakra-ui/layout";
+import {
+	Box,
+	HStack,
+	Container,
+	Center,
+	Text,
+	VStack,
+	Spacer,
+	UnorderedList,
+	ListItem,
+	ListIcon,
+} from "@chakra-ui/layout";
 import React from "react";
+import tanmay from "../assets/tanmay.jpeg";
+
+const Amogh = () => {
+	return (
+		<HStack>
+			<Container>Amogh</Container>
+			<Spacer />
+			<img src="" alt="amogh" />
+		</HStack>
+	);
+};
+
+const Tanmay = () => {
+	return (
+		<HStack>
+			<img
+				style={{ borderRadius: "20px" }}
+				src={tanmay}
+				alt="tanmay"
+				height="400px"
+				width="400px"
+			/>
+			<Spacer />
+			<Container>
+				<UnorderedList>
+					<ListItem>About me?</ListItem>
+					<ListItem>
+						<a href="https://gairTanm.github.io" target="_blank">
+							Website
+						</a>
+					</ListItem>
+					<ListItem>
+						<a
+							href="https://www.github.com/gairTanm"
+							target="_blank"
+						>
+							Github
+						</a>
+					</ListItem>
+					<ListItem>
+						<a
+							target="_blank"
+							href="https://randomtanmay.vercel.app"
+						>
+							Blog
+						</a>
+					</ListItem>
+				</UnorderedList>
+			</Container>
+		</HStack>
+	);
+};
 
 const Team = () => {
 	return (
 		<Box h="100vh" bg="beige" w="100vw">
-			<Text>Team</Text>
+			<Center>
+				<VStack>
+					<Text>Team</Text>
+					<Tanmay />
+					<Amogh />
+				</VStack>
+			</Center>
 		</Box>
 	);
 };
