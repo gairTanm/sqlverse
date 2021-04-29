42cc153ac358d51aa30051700c5cb593437d5428
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-04-30 05:22:04 +0530

    loading animation

--- client/src/components/loading.js
+++ client/src/components/loading.js
@@ -1,11 +1,43 @@
+import { Box, Center } from "@chakra-ui/layout";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import skater from "../assets/skaterwolf.gif";
 
-const Loading = () => {
+const container = {
+	initial: { opacity: 0 },
+	animate: {
+		opacity: 1,
+		transition: {
+			duration: 3,
+			ease: [0.6, -0.05, 0.01, 0.99],
+		},
+	},
+	exit: {
+		opacity: 0,
+	},
+};
+
+const MotionBox = motion(Box);
+
+const Loading = ({ loading }) => {
 	return (
-		<div>
-			<img src={skater} alt="skrrt" />
-		</div>
+		<AnimatePresence>
+			{loading && (
+				<MotionBox
+					variants={container}
+					initial="initial"
+					animate="animate"
+					exit="exit"
+					w="100%"
+					h="100%"
+					bg="#FF7679"
+				>
+					<Center h="100vh">
+						<img src={skater} alt="skrrt" />
+					</Center>
+				</MotionBox>
+			)}
+		</AnimatePresence>
 	);
 };
 
